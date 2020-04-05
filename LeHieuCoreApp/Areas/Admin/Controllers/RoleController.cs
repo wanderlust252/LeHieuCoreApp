8f95115f8acc3d8921b6147d73ceac016e9954fa
Le Dinh Hieu <wanderlust252@gmail.com>
2020-04-06 01:03:32 +0700

    role

--- LeHieuCoreApp/Areas/Admin/Controllers/RoleController.cs
+++ LeHieuCoreApp/Areas/Admin/Controllers/RoleController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LeHieuCoreApp.Application.Interfaces;
+using LeHieuCoreApp.Application.ViewModels.System;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace LeHieuCoreApp.Areas.Admin.Controllers
 {
@@ -26,6 +28,64 @@ namespace LeHieuCoreApp.Areas.Admin.Controllers
 
             return new OkObjectResult(model);
         }
+        [HttpGet]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var model = await _roleService.GetById(id);
 
+            return new OkObjectResult(model);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllPaging(string keyword, int page, int pageSize)
+        {
+            var model = _roleService.GetAllPagingAsync(keyword, page, pageSize);
+            return new OkObjectResult(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveEntity(AppRoleViewModel roleVm)
+        {
+            if (!ModelState.IsValid)
+            {
+                IEnumerable<ModelError> allErrors = ModelState.Values.SelectMany(v => v.Errors);
+                return new BadRequestObjectResult(allErrors);
+            }
+            if (!roleVm.Id.HasValue)
+            {
+                await _roleService.AddAsync(roleVm);
+            }
+            else
+            {
+                await _roleService.UpdateAsync(roleVm);
+            }
+            return new OkObjectResult(roleVm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new BadRequestObjectResult(ModelState);
+            }
+            await _roleService.DeleteAsync(id);
+            return new OkObjectResult(id);
+        }
+
+
+        [HttpPost]
+        public IActionResult ListAllFunction(Guid roleId)
+        {
+            var functions = _roleService.GetListFunctionWithRole(roleId);
+            return new OkObjectResult(functions);
+        }
+
+        [HttpPost]
+        public IActionResult SavePermission(List<PermissionViewModel> listPermmission, Guid roleId)
+        {
+            _roleService.SavePermission(listPermmission, roleId);
+            return new OkResult();
+        }
     }
 }
---
+++ LeHieuCoreApp/wwwroot/app/controllers/role/index.js
@@ -0,0 +1,207 @@
+﻿var RoleController = function () {
+    var self = this;
+
+    this.initialize = function () {
+        loadData();
+        registerEvents();
+    }
+
+    function registerEvents() {
+        //Init validation
+        $('#frmMaintainance').validate({
+            errorClass: 'red',
+            ignore: [],
+            lang: 'vi',
+            rules: {
+                txtName: { required: true }
+            }
+        });
+
+        $('#txt-search-keyword').keypress(function (e) {
+            if (e.which == 13) {
+                e.preventDefault();
+                loadData();
+            }
+        });
+        $("#btn-search").on('click', function () {
+            loadData();
+        });
+
+        $("#ddl-show-page").on('change', function () {
+            lehieu.configs.pageSize = $(this).val();
+            lehieu.configs.pageIndex = 1;
+            loadData(true);
+        });
+
+        $("#btn-create").on('click', function () {
+            resetFormMaintainance();
+            $('#modal-add-edit').modal('show');
+
+        });
+
+        $('body').on('click', '.btn-edit', function (e) {
+            e.preventDefault();
+            var that = $(this).data('id');
+            $.ajax({
+                type: "GET",
+                url: "/Admin/Role/GetById",
+                data: { id: that },
+                dataType: "json",
+                beforeSend: function () {
+                    lehieu.startLoading();
+                },
+                success: function (response) {
+                    var data = response;
+                    $('#hidId').val(data.Id);
+                    $('#txtName').val(data.Name);
+                    $('#txtDescription').val(data.Description);
+                    $('#modal-add-edit').modal('show');
+                    lehieu.stopLoading();
+
+                },
+                error: function (status) {
+                    lehieu.notify('Có lỗi xảy ra', 'error');
+                    lehieu.stopLoading();
+                }
+            });
+        });
+
+        $('#btnSave').on('click', function (e) {
+            if ($('#frmMaintainance').valid()) {
+                e.preventDefault();
+                var id = $('#hidId').val();
+                var name = $('#txtName').val();
+                var description = $('#txtDescription').val();
+
+                $.ajax({
+                    type: "POST",
+                    url: "/Admin/Role/SaveEntity",
+                    data: {
+                        Id: id,
+                        Name: name,
+                        Description: description,
+                    },
+                    dataType: "json",
+                    beforeSend: function () {
+                        lehieu.startLoading();
+                    },
+                    success: function (response) {
+                        lehieu.notify('Update role successful', 'success');
+                        $('#modal-add-edit').modal('hide');
+                        resetFormMaintainance();
+                        lehieu.stopLoading();
+                        loadData(true);
+                    },
+                    error: function () {
+                        lehieu.notify('Has an error', 'error');
+                        lehieu.stopLoading();
+                    }
+                });
+                return false;
+            }
+
+        });
+
+        $('body').on('click', '.btn-delete', function (e) {
+            e.preventDefault();
+            var that = $(this).data('id');
+            lehieu.confirm('Are you sure to delete?', function () {
+                $.ajax({
+                    type: "POST",
+                    url: "/Admin/Role/Delete",
+                    data: { id: that },
+                    beforeSend: function () {
+                        lehieu.startLoading();
+                    },
+                    success: function (response) {
+                        lehieu.notify('Delete successful', 'success');
+                        lehieu.stopLoading();
+                        loadData();
+                    },
+                    error: function (status) {
+                        lehieu.notify('Has an error in deleting progress', 'error');
+                        lehieu.stopLoading();
+                    }
+                });
+            });
+        });
+
+
+    };
+
+   
+    function resetFormMaintainance() {
+        $('#hidId').val('');
+        $('#txtName').val('');
+        $('#txtDescription').val('');
+    }
+
+    function loadData(isPageChanged) {
+        $.ajax({
+            type: "GET",
+            url: "/admin/role/GetAllPaging",
+            data: {
+                keyword: $('#txt-search-keyword').val(),
+                page: lehieu.configs.pageIndex,
+                pageSize: lehieu.configs.pageSize
+            },
+            dataType: "json",
+            beforeSend: function () {
+                lehieu.startLoading();
+            },
+            success: function (response) {
+                var template = $('#table-template').html();
+                var render = "";
+                if (response.RowCount > 0) {
+                    $.each(response.Results, function (i, item) {
+                        render += Mustache.render(template, {
+                            Name: item.Name,
+                            Id: item.Id,
+                            Description: item.Description
+                        });
+                    });
+                    $("#lbl-total-records").text(response.RowCount);
+                    if (render != undefined) {
+                        $('#tbl-content').html(render);
+
+                    }
+                    wrapPaging(response.RowCount, function () {
+                        loadData();
+                    }, isPageChanged);
+
+
+                }
+                else {
+                    $('#tbl-content').html('');
+                }
+                lehieu.stopLoading();
+            },
+            error: function (status) {
+                console.log(status);
+            }
+        });
+    };
+
+    function wrapPaging(recordCount, callBack, changePageSize) {
+        var totalsize = Math.ceil(recordCount / lehieu.configs.pageSize);
+        //Unbind pagination if it existed or click change pagesize
+        if ($('#paginationUL a').length === 0 || changePageSize === true) {
+            $('#paginationUL').empty();
+            $('#paginationUL').removeData("twbs-pagination");
+            $('#paginationUL').unbind("page");
+        }
+        //Bind Pagination Event
+        $('#paginationUL').twbsPagination({
+            totalPages: totalsize,
+            visiblePages: 7,
+            first: 'Đầu',
+            prev: 'Trước',
+            next: 'Tiếp',
+            last: 'Cuối',
+            onPageClick: function (event, p) {
+                lehieu.configs.pageIndex = p;
+                setTimeout(callBack(), 200);
+            }
+        });
+    }
+}
