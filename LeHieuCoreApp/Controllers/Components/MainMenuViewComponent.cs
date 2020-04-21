1641d25ab64717f933a53f5662edf887254350ac
Le Dinh Hieu <wanderlust252@gmail.com>
2020-04-22 01:07:23 +0700

    ls44

---
+++ LeHieuCoreApp/Controllers/Components/MainMenuViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeHieuCoreApp.Controllers.Components
+{
+    public class MainMenuViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
